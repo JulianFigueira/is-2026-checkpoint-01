dd8e94e43c1d6e31e75a03ad4a4229ddce9c7fbc
yamiltundis <yamiltundis6@gmail.com>
2026-04-22 13:49:56 -0300

    feat(backend) : Armo el endpoint '/api/team/' que devuelve la info de los integrantes del grupo trayendo los datos desde la BD

--- backend/app.py
+++ backend/app.py
@@ -10,3 +10,23 @@ DB_URL = os.environ.get('DATABASE_URL')
 # Funcion para conectarse con la BD
 def conexion_bd():
     return psycopg2.connect(DB_URL)
+
+@app.route('/api/team', methods=['GET'])
+def traer_equipo():
+    try:
+        # 1. Conexión con la BD
+        conn = conexion_bd()
+        cur = conn.cursor()
+        
+        # 2. Ejecución de la consulta
+        cur.execute("SELECT * FROM members;")
+        rows = cur.fetchall()
+        
+        # 3. Cierre de la conexión
+        cur.close()
+        conn.close()
+        
+        # 4. Respuesta
+        return jsonify({"members": rows}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
