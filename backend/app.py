2e106de07c8a36087ed8b6b448b049797ba5b26a
yamiltundis <yamiltundis6@gmail.com>
2026-04-23 14:21:52 -0300

    fix(backend) : corrijo los puertos del backend (del 4000 al 5000)

--- backend/app.py
+++ backend/app.py
@@ -61,4 +61,4 @@ def get_info():
     return jsonify(metadata), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=4000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
